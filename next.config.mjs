82b3876ce3814a24fc38cb8368c9c6d97d84e5c0
Terry Kim <56363632+thkim0118@users.noreply.github.com>
2024-09-08 16:31:22 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const prefix =
+  process.env.NODE_ENV === 'production' ? 'https://thkim0118.github.io/wedding-invitation-letter/' : ''
+
+const nextConfig = {
+  output: 'export',
+  assetPrefix: prefix,
+};
 
 export default nextConfig;
